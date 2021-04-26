846d5643b61d0a2fe5bc8594451b4389a4260723
AndresCastro02 <83243924+AndresCastro02@users.noreply.github.com>
2021-04-26 15:43:19 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,10 @@
+<?php 
+
+if (isset($_POST['datos'])){
+	$tarea = $_POST['tarea'];
+	echo $tarea;
+	}
+
+
+
+ ?>
---
+++ proyecto.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LISTA DE TAREAS</title>
+</head>
+<body>
+<h1>Mi lista de tareas</h1>
+</body>
+</html>
+
+	<div
+
+
+	class="Nombre tarea">
+	<form action="datos.php" method="POST">
+		<div>
+			<input type="text" name="tarea" class="form-control" placeholder="Escriba su tarea">
+		</div>
+		<button type="submit" name="guardar_tarea">guardar</button>
+
+	 </form>
+
+	</div> 
+
+
+
+
+
+
+
+
